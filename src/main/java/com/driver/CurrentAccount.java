236a8fca79c925fcda2c261b912c9e63390cdb8b
Pavan Bhat <122112767+PavanAgasal@users.noreply.github.com>
2023-06-17 20:57:28 +0530

    CurrentAccount.java

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,19 +1,40 @@
 package com.driver;
 
-public class CurrentAccount extends BankAccount{
-    String tradeLicenseId; //consists of Uppercase English characters only
+public class CurrentAccount extends BankAccount {
+    String tradeLicenseId; // consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name, balance, 5000.0); // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
-        // A trade license Id is said to be valid if no two consecutive characters are same
-        // If the license Id is valid, do nothing
-        // If the characters of the license Id can be rearranged to create any valid license Id
-        // If it is not possible, throw "Valid License can not be generated" Exception
+        char[] licenseChars = tradeLicenseId.toCharArray();
+        int length = licenseChars.length;
 
+        for (int i = 0; i < length - 1; i++) {
+            if (licenseChars[i] == licenseChars[i + 1]) {
+                rearrangeLicenseId(licenseChars, i);
+                return;
+            }
+        }
+
+        throw new Exception("Valid License cannot be generated");
     }
 
+    private void rearrangeLicenseId(char[] licenseChars, int index) {
+        int length = licenseChars.length;
+
+        // Swap the current and next character
+        char temp = licenseChars[index];
+        licenseChars[index] = licenseChars[index + 1];
+        licenseChars[index + 1] = temp;
+
+        // Swap the last character with any other character
+        temp = licenseChars[length - 1];
+        licenseChars[length - 1] = licenseChars[index];
+        licenseChars[index] = temp;
+
+        tradeLicenseId = new String(licenseChars);
+    }
 }
