5fda206a2ab6a3a8fd09d47001153ba8a5bfe9fc
Pavan Bhat <122112767+PavanAgasal@users.noreply.github.com>
2023-06-17 20:56:03 +0530

    BankAccount.java

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.Random;
+
 public class BankAccount {
 
     private String name;
@@ -7,25 +9,37 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
+    public String generateAccountNumber(int digits, int sum) throws Exception {
+        Random random = new Random();
+        StringBuilder accountNumber = new StringBuilder();
+        int currentSum = 0;
 
-        return null;
+        for (int i = 0; i < digits; i++) {
+            int digit = random.nextInt(10);
+            accountNumber.append(digit);
+            currentSum += digit;
+        }
+
+        if (currentSum != sum) {
+            throw new Exception("Account Number cannot be generated");
+        }
+
+        return accountNumber.toString();
     }
 
     public void deposit(double amount) {
-        //add amount to balance
-
+        balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if (balance - amount < minBalance) {
+            throw new Exception("Insufficient Balance");
+        }
+        balance -= amount;
     }
-
 }
